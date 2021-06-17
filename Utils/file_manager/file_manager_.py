0b42dae64dda14ab245d6ca4161fbe6f17be8db4
Elvis Ohemeng Gyaase <48416104+StepLogic@users.noreply.github.com>
2021-06-17 20:58:23 +0000

    modified export

--- Utils/file_manager/file_manager_.py
+++ Utils/file_manager/file_manager_.py
@@ -8,10 +8,14 @@ def clean_up_after_datcom():
     shutil.move("datcom.out", datcom_output_file)
     os.remove("for013.dat")
     os.remove("for014.dat")
+
+
 def clean_after_moveables(filename):
     import shutil
     shutil.move(f"{filename}.obj", f"{model_dir}/moveables/{filename}.obj")
     os.remove(f"{filename}.stl")
+
+
 def clean_after_body(filename):
     import shutil
     shutil.move(f"{filename}.obj", f"{model_dir}/body/{filename}.obj")
